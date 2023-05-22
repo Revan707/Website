c26a11eabac56b58c5baf363647cab9b8a6f95d0
Revan707 <ravangh@code.edu.az>
2023-05-22 14:06:49 +0400

    Task isn't done

---
+++ Assets/main.js
@@ -0,0 +1,10 @@
+const container=document.querySelector(".container")
+const bodyEnd=querySelector(".body_end")
+const images=document.querySelector(".images")
+const firstPage=querySelector(".first_page")
+const secondPage=querySelector(".second_page")
+const thirdPage=querySelector(".third_page")
+
+
+
+
